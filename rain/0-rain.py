ca288fff904089cf026c4d23ca40bc5bb2392fed
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-10-08 23:50:39 +0200

    fixed pep 8 style

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-0-rain.py
+rain.py
 """
 
 def rain(walls):
@@ -13,7 +13,6 @@ def rain(walls):
 
     Returns:
         int: Total amount of rainwater retained.
-
     """
     if len(walls) == 0:
         return 0
@@ -23,11 +22,11 @@ def rain(walls):
 
     left_max[0] = walls[0]
     for i in range(1, len(walls)):
-        left_max[i] = max(left_max[i-1], walls[i])
+        left_max[i] = max(left_max[i - 1], walls[i])
 
     right_max[-1] = walls[-1]
-    for i in range(len(walls)-2, -1, -1):
-        right_max[i] = max(right_max[i+1], walls[i])
+    for i in range(len(walls) - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], walls[i])
 
     total_water = 0
     for i in range(len(walls)):
@@ -35,7 +34,6 @@ def rain(walls):
         total_water += water_height
 
     return total_water
-
 
 if __name__ == "__main__":
     walls = [0, 1, 0, 2, 0, 3, 0, 4]
